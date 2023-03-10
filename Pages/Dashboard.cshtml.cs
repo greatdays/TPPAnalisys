674e3967cc29f44eac5f9e52681a5e5a3c67b7d5
Ananthalvan Soundararajan <ananthalvan.soundararajan@lacity.org>
2023-03-10 22:51:07 +0000

    Dashboard - sample data populated - Mar 10

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -8,7 +8,16 @@ namespace DeveloperPortal.Pages
     public class DashboardModel : PageModel
     {
         public IConfigurationRoot Configuration { get; set; }
-        public int RecordCount { get; set; }
+        public int NewProjRecordCount { get; set; }
+        public List<string?> PlanReviewList { get; private set; }
+        public int PlanReviewRecordCount { get; set; }
+        public List<string?> SiteInspectionList { get; private set; }
+        public int SiteInspectionRecordCount { get; set; }
+        public List<string?> NACInspectionList { get; private set; }
+        public int NACRecordCount { get; set; }
+        public List<string?> CompletedCertList { get; private set; }
+        public int CompletedCertRecordCount { get; set; }
+        
 
         public List<string?> NewProjectsList { get; set; } = new List<string?>();
         //public Startup(IConfigurationRoot configuration) { }
@@ -17,9 +26,46 @@ namespace DeveloperPortal.Pages
             var configBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             Configuration = configBuilder.Build();
+
+            DataTable dt = GetAllConstructionCases();
             
-            string? connString = Configuration.GetConnectionString("AAHR");            
-            SqlConnection conn  = new SqlConnection(connString);
+            //TODO: Filter conditions to be revised
+            DataRow[] drNewProjColl = dt.Select("Status='Under Document Review'"); //Type='New Construction Project' AND
+            DataRow[] drPlanReviewColl = dt.Select("Status='Ready for Design Review'");
+            DataRow[] drSiteInspectionColl = dt.Select("Status='Site Case In Progress'");
+            DataRow[] drNACColl = dt.Select("Status='NAC Inspection IN Progress'");
+            DataRow[] drCompletedCertColl = dt.Select("Status='City Compliance Certificate Issued'");
+
+            //New Projects
+            NewProjectsList = drNewProjColl.Select(x => x["ProjectName"].ToString()).ToList();
+            NewProjectsList.Sort();
+            NewProjRecordCount = NewProjectsList.Count;
+
+            //Plan Review
+            PlanReviewList = drPlanReviewColl.Select(x => x["ProjectName"].ToString()).ToList();
+            PlanReviewList.Sort();
+            PlanReviewRecordCount = PlanReviewList.Count;
+
+            //Site Inspection
+            SiteInspectionList = drSiteInspectionColl.Select(x => x["ProjectName"].ToString()).ToList();
+            SiteInspectionList.Sort();
+            SiteInspectionRecordCount = SiteInspectionList.Count;
+
+            //NAC
+            NACInspectionList = drNACColl.Select(x => x["ProjectName"].ToString()).ToList();
+            NACInspectionList.Sort();
+            NACRecordCount = NACInspectionList.Count;
+
+            //Completed Cert
+            CompletedCertList = drCompletedCertColl.Select(x => x["ProjectName"].ToString()).ToList();
+            CompletedCertList.Sort();
+            CompletedCertRecordCount = CompletedCertList.Count;
+        }
+
+        private DataTable GetAllConstructionCases()
+        {
+            string? connString = Configuration.GetConnectionString("AAHR");
+            SqlConnection conn = new SqlConnection(connString);
             conn.Open();
             SqlCommand comm = conn.CreateCommand();
             comm.CommandText = "[AAHPCC].[uspRoGetAllConstructionCases]";
@@ -30,18 +76,7 @@ namespace DeveloperPortal.Pages
             dt.Load(dr);
 
             conn.Close();
-
-            
-
-            //Console.WriteLine(dt.Rows.Count);
-            RecordCount = dt.Rows.Count;
-            DataRow[] drNewProjColl = dt.Select("Status='Under Document Review'"); //Type='New Construction Project' AND
-            List<string?> lstNewProjects = new List<string?>();
-            //lstNewProjects = drNewProjColl.ToList();
-
-            NewProjectsList = drNewProjColl.Select(x => x["ProjectName"].ToString()).ToList();
-            NewProjectsList.Sort();
-            RecordCount = NewProjectsList.Count;
+            return dt;
         }
     }
 }
