fd36e92d54948c581b11bd21561d6888dbad3ae1
Murtaza Rokerya <murtaza.rokerya@lacity.org>
2023-03-20 17:05:43 -0700

    Change the solution folder structure

---
+++ DeveloperPortal.Models/CryptoHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperPortal.Models
+{
+   public static  class CryptoHelper
+    {
+        private static string passPhrase = "Pas5pr@se"; // can be any string
+        private static string saltValue = "{3Di-Inc-USA}";        // can be any string
+        private static string hashAlgorithm = "SHA1";             // can be "MD5"
+        private static int passwordIterations = 2;                // can be any number
+        private static string initVector = "@1B2c3D4e5F6g7H8"; // must be 16 bytes
+        private static int keySize = 256;                // can be 192 or 128
+
+        public static string Encrypt(string plainText)
+        {
+            string cipherText = RijndaelSymmetricKey.Encrypt
+           (
+               plainText,
+               passPhrase,
+               saltValue,
+               hashAlgorithm,
+               passwordIterations,
+               initVector,
+               keySize
+           );
+            return cipherText;
+        }
+        public static string Decrypt(string cipherText)
+        {
+            string plainText = RijndaelSymmetricKey.Decrypt
+            (
+                cipherText,
+                passPhrase,
+                saltValue,
+                hashAlgorithm,
+                passwordIterations,
+                initVector,
+                keySize
+            );
+            return plainText;
+        }
+    }
+}
---
+++ DeveloperPortal.Models/JsonHelpers.cs
@@ -0,0 +1,53 @@
+﻿namespace DeveloperPortal.Models
+{
+    public class JsonData<T>
+    {
+        public JsonData()
+        {
+            Result = default(T);
+            EventMessages = new List<EventData>();
+        }
+
+        public JsonData(T result)
+        {
+            Result = result;
+            EventMessages = new List<EventData>();
+        }
+
+        public List<EventData> EventMessages { get; set; }
+        public T Result { get; set; }
+    }
+
+    public class EventData
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Class { get; set; }
+
+        public EventData()
+        {
+            this.Key = default(string);
+            this.Value = default(string);
+            this.Class = default(string);
+        }
+
+        public EventData(string key, string value, string className)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.Class = className;
+        }
+    }
+
+    /// <summary>
+    /// Using this object to construct a json model for passing the status of an action plus a confirmation message which will be used in model popup.
+    /// </summary>
+    public class JsonStatus
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
