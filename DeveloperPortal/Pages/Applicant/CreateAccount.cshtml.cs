1db064eed85183a0984f60a8a02bab978e130f5d
Ananthalvan Soundararajan <ananthalvan.soundararajan@lacity.org>
2023-06-12 10:13:30 -0700

    Signup page - UI - code changes. Incremental Check-in

---
+++ DeveloperPortal/Pages/Applicant/CreateAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeveloperPortal.Pages.Applicant
+{
+    public class CreateAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
