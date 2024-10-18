56365463062b9b72a5c88f9fabfafebb05a71728
Ananthalvan Soundararajan <ananthalvan.soundararajan@lacity.org>
2024-10-18 16:09:50 -0700

    Upgraded the solution to .NET 8.0

---
+++ DeveloperPortal/Pages/Shared/GoogleTranslate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeveloperPortal.Pages.Shared
+{
+    public class GoogleTranslateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
