4c113621230be0465ef19afc273a7fc20899aeb4
Jigneshkumar Hirpara <jigneshkumar.hirpara@lacity.org>
2024-11-14 08:21:37 -0800

    Made changes for Landing page UI and other  some common UI  changes -Dashboard -Landing

---
+++ UI/DeveloperPortal/Pages/ProjectDetails/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeveloperPortal.Pages.ProjectDetails
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ UI/DeveloperPortal/Pages/ProjectDetails/ProjectDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeveloperPortal.Pages.ProjectDetails
+{
+    public class ProjectDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
