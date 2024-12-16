e085fc27cf9ee8a9e2ad96092ca7fb1be88aa658
Jigneshkumar Hirpara <jigneshkumar.hirpara@lacity.org>
2024-12-16 07:30:13 -0800

    Moved javascript to specific pages and fixed AJax call issue

---
+++ UI/DeveloperPortal/wwwroot/js/customcommon.js
@@ -0,0 +1,26 @@
+﻿/* Function LoadPartialView
+ *** This function is used for load partial view in div.***
+--------------------------------------------------------------------------------------------------------------------------*/
+function LoadPartialView(actionPath, parameter, containerName) {
+    AjaxCommunication.CreateRequest(this.window, "GET", actionPath, 'html', parameter,
+        function (data) {
+            $(containerName).html(data);
+        },
+        function (xhr, status) {
+            alert(status);
+        }, true, null, false);
+}
+
+/* Function LoadPopUpPartialView
+***This function is used for load partial view in popUp div.*** 
+--------------------------------------------------------------------------------------------------------------------------*/
+function LoadPopUpPartialView(actionPath, parameter, containerName) {
+    AjaxCommunication.CreateRequest(this.window, "GET", actionPath, 'html', parameter,
+        function (data) {
+            $(containerName).html(data).modal('show');
+            return false;
+        },
+        function (xhr, status) {
+            alert(status);
+        }, true, null, false);
+}
