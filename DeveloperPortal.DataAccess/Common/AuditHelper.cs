f51f8812e9309f960b5660a93aad4e721598b459
PSuryawanshi <parameshwar.suryawanshi@lacity.org>
2025-06-26 18:23:00 +0530

    STSK0011373-Refactor the Project List Table Component to Adhere to the New Repository Pattern

---
+++ DeveloperPortal.DataAccess/Common/AuditHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperPortal.DataAccess.Common
+{
+    public static class AuditHelper
+    {
+        public static void ApplyAudit(object entity, EntityState state, string username)
+        {
+            if (string.IsNullOrWhiteSpace(username)) return;
+
+            var entityType = entity.GetType();
+            var now = DateTime.UtcNow;
+
+            var createdOn = entityType.GetProperty("CreatedOn");
+            var createdBy = entityType.GetProperty("CreatedBy");
+            var modifiedOn = entityType.GetProperty("ModifiedOn");
+            var modifiedBy = entityType.GetProperty("ModifiedBy");
+
+            if (state == EntityState.Added)
+            {
+                createdOn?.SetValue(entity, now);
+                createdBy?.SetValue(entity, username);
+            }
+
+            if (state == EntityState.Added || state == EntityState.Modified)
+            {
+                modifiedOn?.SetValue(entity, now);
+                modifiedBy?.SetValue(entity, username);
+            }
+        }
+    }
+}
---
+++ DeveloperPortal.DataAccess/Entity/Data/AAHREntities.Audit.cs
@@ -0,0 +1,64 @@
+﻿using DeveloperPortal.DataAccess.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperPortal.DataAccess.Entity.Data;
+
+public partial class AAHREntities : DbContext
+{
+
+    public async Task<int> SaveChangesWithAuditAsync(string username)
+    {
+        ApplyAuditDetails(username);
+        return await base.SaveChangesAsync();
+    }
+
+    private void ApplyAuditDetails(string username)
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+        foreach (var entry in entries)
+        {
+            AuditHelper.ApplyAudit(entry.Entity, entry.State, username);
+        }
+    }
+
+    public int SaveChanges(string username)
+    {
+        ApplyAuditInfo(username);
+
+        return base.SaveChanges();
+    }
+    private void ApplyAuditInfo(string username)
+    {
+
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+        foreach (var entry in entries)
+        {
+            var entity = entry.Entity;
+            var entityType = entity.GetType();
+
+            var modifiedOnProp = entityType.GetProperty("ModifiedOn");
+            var modifiedByProp = entityType.GetProperty("ModifiedBy");
+            var createdOnProp = entityType.GetProperty("CreatedOn");
+            var createdByProp = entityType.GetProperty("CreatedBy");
+
+            if (modifiedOnProp != null && modifiedOnProp.PropertyType == typeof(DateTime))
+                modifiedOnProp.SetValue(entity, DateTime.UtcNow);
+
+            if (modifiedByProp != null && modifiedByProp.PropertyType == typeof(string))
+                modifiedByProp.SetValue(entity, username);
+
+            if (entry.State == EntityState.Added)
+            {
+                if (createdOnProp != null && createdOnProp.PropertyType == typeof(DateTime))
+                    createdOnProp.SetValue(entity, DateTime.UtcNow);
+
+                if (createdByProp != null && createdByProp.PropertyType == typeof(string))
+                    createdByProp.SetValue(entity, username);
+            }
+        }
+    }
+}
