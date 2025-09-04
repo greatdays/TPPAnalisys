ac54bfc53195333d565122425d739e1c2824bd9a
Deebin Babu <deebin.babu@lacity.org>
2025-09-03 21:29:06 -0700

    Adding FloorPlanType TPP

---
+++ DeveloperPortal.Application/ProjectDetail/Interface/IFloorPlanTypeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using DeveloperPortal.DataAccess.Entity.Models.Generated;
+using DeveloperPortal.Domain.ProjectDetail;
+
+namespace DeveloperPortal.Application.ProjectDetail.Interface
+{
+    public interface IFloorPlanTypeService
+    {
+        Task<FloorPlanTypeModel> GetFloorPlanTypes(int siteId, int projectId);
+        Task<List<LutTotalBedrooms>> GetLutTotalBedrooms();
+        Task<List<LutTotalBathrooms>> GetLutTotalBathrooms();
+        Task<List<LutBathroomTypes>> GetLutBathroomType();
+        Task<List<LutTotalBathroomTypeOption>> GetLutBathroomTypeOption();
+        Task<int> AddFloorPlanTypeComplianceMatrix(FloorPlanTypeModel floorPanTypeModel);
+        List<FloorPlanTypeModel> GetFloorPlanInformation(int ProjectID);
+        Task<FloorPlanTypeModel> FetchFloorPlanById(int floorplanId);
+        bool EditFloorPlanType(FloorPlanTypeModel floorPlanTypeModel);
+        bool DeleteFloorPlantype(int floorPlanId, string userName);
+
+    }
+}
