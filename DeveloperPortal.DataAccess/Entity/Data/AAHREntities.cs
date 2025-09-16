b86b0e2d519814bddea516260d11644ba40c6a0e
LAHDSYSTEMS\ekim <eric.kim@lacity.org>
2025-09-16 00:07:20 -0700

    Modified settings for local db

--- DeveloperPortal.DataAccess/Entity/Data/AAHREntities.cs
+++ DeveloperPortal.DataAccess/Entity/Data/AAHREntities.cs
@@ -1213,7 +1213,7 @@ public partial class AAHREntities : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=43devdb10;Encrypt=False;Trust Server Certificate=true;initial catalog=AAHRDev;persist security info=True;user id=appACHP;password=BDpwD7@cHP;multipleactiveresultsets=True");
+        => optionsBuilder.UseSqlServer("Data Source=43devdb10;Encrypt=False;Trust Server Certificate=true;initial catalog=AAHRLocal;persist security info=True;user id=appACHP;password=BDpwD7@cHP;multipleactiveresultsets=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
