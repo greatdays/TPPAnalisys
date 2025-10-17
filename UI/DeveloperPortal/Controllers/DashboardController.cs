22d4669d6749f0fb6c515126a6afb1d6b9ceefc6
SSingh <sumer.singh@lacity.org>
2025-10-17 11:26:55 -0700

    Changes to verify

--- UI/DeveloperPortal/Controllers/DashboardController.cs
+++ UI/DeveloperPortal/Controllers/DashboardController.cs
@@ -11,6 +11,8 @@ using DeveloperPortal.Application.ProjectDetail.Implementation;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+
+
 namespace DeveloperPortal.Controllers
 {
     //[Route("api/[controller]")]
